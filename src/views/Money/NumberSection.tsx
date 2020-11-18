8c4a2dc57e35bc0d70d756513aba977c6300cdca
张文川 <13522750636@163.com>
2020-11-18 21:50:57 +0800

    numberPad功能 (work in process)

--- src/views/Money/NumberSection.tsx
+++ src/views/Money/NumberSection.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const NumberSection = styled.section`
+const Wrapper = styled.section`
   >.money {
     font-size: 36px;
     line-height: 72px;
@@ -32,5 +33,64 @@ const NumberSection = styled.section`
     }
   }
 `;
+
+const NumberSection: React.FC = () => {
+  const [output, setOutput] = useState('0')
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if (text === null) { return }
+    switch (text) {
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '0':
+        if (output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break;
+      case '.':
+        console.log(text)
+        break;
+      case '删除':
+        console.log(text)
+        break;
+      case '清空':
+        console.log(text)
+        break;
+      case 'OK':
+        console.log(text)
+        break;
+    }
+  }
+  return (
+    <Wrapper>
+      <div className="money">{output}</div>
+      <div className="pad clearfix" onClick={onClickButtonWrapper}>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  )
+}
 
 export { NumberSection };
