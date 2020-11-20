7f02086c0a933d7cd1351c0656bc1d86de08e2f1
张文川 <13522750636@163.com>
2020-11-20 11:27:24 +0800

    抽离useTags

--- src/views/Money/TagSection.tsx
+++ src/views/Money/TagSection.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
+import { useTags } from "../useTags";
 
 const Wrapper = styled.section`
   background-color: #fff;
@@ -37,7 +38,7 @@ type Props = {
 }
 
 const TagSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const { tags, setTags } = useTags()
   const selectTags = props.value
   const onAddTags = () => {
     const tagName = window.prompt('新标签的名称为');
--- src/views/tags.tsx
+++ src/views/tags.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Layout from "components/Layout";
+import { useTags } from "./useTags"
 
 function Tags() {
+  const { tags, setTags } = useTags()
   return (
     <Layout>
-      <h2>Tags</h2>
+      <ol>
+        {
+          tags.map(tag =>
+            <li key={tag}>{tag}</li>
+          )
+        }
+      </ol>
     </Layout>
-  ) 
+  )
 }
 
 export default Tags;
