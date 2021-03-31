61a5bd73674c5f4292ff28603681578ea6ea1fee
zhangwenchuan <13522750636@163.com>
2021-03-31 19:37:53 +0800

    feat(pakage): 更新pakage

--- src/components/useTags.tsx
+++ src/components/useTags.tsx
@@ -11,16 +11,10 @@ export const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0]
   const updateTags = (obj: { id: number, name: string }) => {
-    const index = tags.findIndex(tag => tag.id === obj.id)
-    const tagsClone = JSON.parse(JSON.stringify(tags)) // 深拷贝
-    tagsClone.splice(index, 1, { ...obj })
-    setTags(tagsClone)
+    setTags(tags.map(tag => tag.id === obj.id ? obj : tag))
   }
   const deleteTag = (id: number) => {
-    const index = tags.findIndex(tag => tag.id === id)
-    const tagsClone = JSON.parse(JSON.stringify(tags)) // 深拷贝
-    tagsClone.splice(index, 1)
-    setTags(tagsClone)
+    setTags(tags.filter(tag => tag.id !== id))
   }
   return { tags, setTags, findTag, updateTags, deleteTag }
 }
